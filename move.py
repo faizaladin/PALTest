78fda0f265498e3ecc376ec26974ab41651af34c
faizaladin <faizaladin365@gmail.com>
2024-04-26 21:45:29 -0400

    directions check

--- move.py
+++ move.py
@@ -23,8 +23,25 @@ def backward(num):
     motor_a.stop()
     motor_b.stop()
 
+def left(num):
+    motor_a.forward()
+    motor_b.backward()
+    time.sleep(num)
+    motor_a.stop()
+    motor_b.stop()
 
-backward(3)
+def right(num):
+    motor_a.backward()
+    motor_b.forward()
+    time.sleep(num)
+    motor_a.stop()
+    motor_b.stop()
+
+forward(2)
 time.sleep(1)
-forward(3)
+left(2)
+time.sleep(1)
+right(2)
+time.sleep(1)
+backward(2)
 
