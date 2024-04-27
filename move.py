b4ee39e0ccda819ccad920d9a2670060152349d8
faizaladin <faizaladin365@gmail.com>
2024-04-26 21:31:19 -0400

    trying gpiozero

--- move.py
+++ move.py
@@ -1,34 +1,19 @@
-import RPi.GPIO as GPIO
+from gpiozero import Motor, PWMOutputDevice
 import time
 
-GPIO.setmode(GPIO.BCM)
+ena = PWMOutputDevice(12)
+enb = PWMOutputDevice(13)
+motor_a = Motor(forward=17, backward=27)
+motor_b = Motor(forward=22, backward=5)
 
-ena = 12
-enb = 13
-in1 = 17
-in2 = 27
-in3 = 22
-in4 = 5
-
-GPIO.setup(in1, GPIO.OUT)
-GPIO.setup(in2, GPIO.OUT)
-GPIO.setup(ena, GPIO.OUT)
-GPIO.setup(in3, GPIO.OUT)
-GPIO.setup(in4, GPIO.OUT)
-GPIO.setup(enb, GPIO.OUT)
-
-p = GPIO.PWM(ena, 1000)
-s = GPIO.PWM(enb, 1000)
+ena.value = 1.0
+enb.value = 1.0
 
 def forward(num):
-    GPIO.output(in1, GPIO.LOW)
-    GPIO.output(in2, GPIO.HIGH)
-    GPIO.output(in3, GPIO.HIGH)
-    GPIO.output(in4, GPIO.LOW)
+    motor_a.forward()
+    motor_b.forward()
     time.sleep(num)
-    GPIO.output(in1, GPIO.HIGH)
-    GPIO.output(in2, GPIO.HIGH)
-    GPIO.output(in3, GPIO.HIGH)
-    GPIO.output(in4, GPIO.HIGH)
+    motor_a.stop()
+    motor_b.stop()
 
 forward(2)
