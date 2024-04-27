94520a571841e2b86ef4a31d3aa26ecc461a2ff6
faizaladin <faizaladin365@gmail.com>
2024-04-26 21:40:43 -0400

    move slower and incorporate backwards

--- move.py
+++ move.py
@@ -6,8 +6,8 @@ enb = PWMOutputDevice(13)
 motor_a = Motor(forward=17, backward=27)
 motor_b = Motor(forward=5, backward=22)
 
-ena.value = 1.0
-enb.value = 1.0
+ena.value = 0.5
+enb.value = 0.5
 
 def forward(num):
     motor_a.forward()
@@ -16,4 +16,15 @@ def forward(num):
     motor_a.stop()
     motor_b.stop()
 
+def backward(num):
+    motor_a.backward()
+    motor_b.backward()
+    time.sleep(num)
+    motor_a.stop()
+    motor_b.stop()
+
+
+backward(3)
+time.sleep(1)
 forward(2)
+
