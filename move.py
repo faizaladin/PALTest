f61623b8c32b583c0138919fc7a00f57813a741c
faizaladin <faizaladin365@gmail.com>
2024-04-26 22:10:59 -0400

    final one

--- move.py
+++ move.py
@@ -45,22 +45,22 @@ def left(num):
 
 forward(1.5)
 time.sleep(1)
-left(1.15)
+left(1.2)
 time.sleep(1)
 
 forward(1.5)
 time.sleep(1)
-left(1.15)
+left(1.2)
 time.sleep(1)
 
 forward(1.5)
 time.sleep(1)
-left(1.15)
+left(1.2)
 time.sleep(1)
 
 forward(1.5)
 time.sleep(1)
-left(1.15)
+left(1.2)
 time.sleep(1)
 
 
