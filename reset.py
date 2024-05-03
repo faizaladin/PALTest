d6de76e1d4cb1760a3743d245786e28ee7fdcefc
faizaladin <faizaladin365@gmail.com>
2024-05-03 19:26:41 -0400

    changed threshold

--- reset.py
+++ reset.py
@@ -6,9 +6,9 @@ import random
 
 robot_info = testcamera.calculate_orientation()
 
-while robot_info[1] > 2 and robot_info[1] < 358:
+while robot_info[1] > 5 and robot_info[1] < 355:
     print(robot_info[1])
-    if robot_info[1] < 2 or robot_info[1] > 358:
+    if robot_info[1] < 5 or robot_info[1] > 355:
         break
     elif robot_info[1] > 90:
         move.right(random.uniform(0.1, 0.5), 0.87)
