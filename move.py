bfcdc5deea402bf4cf272186fc4cafc05fee206b
faizaladin <faizaladin365@gmail.com>
2024-05-03 16:16:50 -0400

    test

--- move.py
+++ move.py
@@ -31,16 +31,17 @@ def right(num):
     motor_b.backward()
     time.sleep(num)
 
-def left(num, ena_value, enb_value):
-    ena.value = ena_value
-    enb.value = enb_value
-    motor_a.forward()
+def left(num, en_value):
+    ena.value = en_value
+    enb.value = en_value
+    motor_a.backward()
     motor_b.forward()
     time.sleep(num)
 
 def curve_left_while_forward():
-    #forward(0.5, 0.5)
-    left(2, 0.0000000001, 1) 
+    forward(0.25, 0.5)  # Move forward for 1 second at 50% speed
+    left(1.5, 1) 
+    forward(1, 0.5)   # 
     stop()
 
 curve_left_while_forward()
