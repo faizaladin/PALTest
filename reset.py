be6cf24fe9bac9553e1ce78373b512558149689c
faizaladin <faizaladin365@gmail.com>
2024-05-03 17:10:57 -0400

    rip

--- reset.py
+++ reset.py
@@ -3,7 +3,7 @@ import move
 
 robot_info = testcamera.calculate_orientation()
 
-while robot_info[1] > 2 or robot_info[1] < 358:
+while robot_info[1] > 2 and robot_info[1] < 358:
     if robot_info[1] > 90:
         move.right(0.5, 1)
         move.stop()
