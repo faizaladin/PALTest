ca38918360fa693ddfd187fa8a6d05da294121fa
faizaladin <faizaladin365@gmail.com>
2024-05-03 16:32:34 -0400

    all turns

--- move.py
+++ move.py
@@ -50,4 +50,42 @@ def curve_left_while_forward250():
     forward(1, 0.5)   # 
     stop()
 
-curve_left_while_forward125()
+def curve_left_while_forward375():
+    forward(0.25, 0.5)  # Move forward for 1 second at 50% speed
+    left(0.95, 1) 
+    forward(1, 0.5)   # 
+    stop()
+
+def curve_left_while_forward500():
+    forward(0.25, 0.5)  # Move forward for 1 second at 50% speed
+    left(0.80, 1) 
+    forward(1, 0.5)   # 
+    stop()
+
+def curve_left_while_forward625():
+    forward(0.25, 0.5)  # Move forward for 1 second at 50% speed
+    left(0.65, 1) 
+    forward(1, 0.5)   # 
+    stop()
+
+def curve_left_while_forward750():
+    forward(0.25, 0.5)  # Move forward for 1 second at 50% speed
+    left(0.50, 1) 
+    forward(1, 0.5)   # 
+    stop()
+
+def curve_left_while_forward875():
+    forward(0.25, 0.5)  # Move forward for 1 second at 50% speed
+    left(0.35, 1) 
+    forward(1, 0.5)   # 
+    stop()
+
+def curve_left_while_forward1000():
+    forward(0.25, 0.5)  # Move forward for 1 second at 50% speed
+    left(0.2, 1) 
+    forward(1, 0.5)   # 
+    stop()
+
+
+
+curve_left_while_forward250()
