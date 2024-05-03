d5d5723444abd9175e2fe30e7f255bb2b541974c
faizaladin <faizaladin365@gmail.com>
2024-05-03 17:04:13 -0400

    smal fix

--- reset.py
+++ reset.py
@@ -4,7 +4,7 @@ import move
 robot_info = testcamera.calculate_orientation()
 
 while robot_info[1] > 2 or robot_info[1] < 358:
-    if robot_info[2] > 90:
+    if robot_info[1] > 90:
         move.right(0.5)
     else:
         move.left(0.5)
