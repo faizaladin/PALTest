89d81c6e079820c4ab5c026cf46c2483f7dc3bac
faizaladin <faizaladin365@gmail.com>
2024-05-03 17:03:44 -0400

    what the fuck

--- reset.py
+++ reset.py
@@ -3,4 +3,11 @@ import move
 
 robot_info = testcamera.calculate_orientation()
 
+while robot_info[1] > 2 or robot_info[1] < 358:
+    if robot_info[2] > 90:
+        move.right(0.5)
+    else:
+        move.left(0.5)
+    robot_info = testcamera.calculate_orientation()
+
 print(robot_info)
