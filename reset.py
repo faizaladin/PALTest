fa9816181897a3f7941f2ffd3de874372f6d2103
faizaladin <faizaladin365@gmail.com>
2024-05-03 17:28:11 -0400

    changing rand values

--- reset.py
+++ reset.py
@@ -10,10 +10,10 @@ while True:
         while robot_info[1] > 5 and robot_info[1] < 355:
             print(robot_info)
             if robot_info[1] > 90:
-                move.right(random.uniform(0.1, 1), 1)
+                move.right(random.uniform(0.1, 0.5), 0.87)
                 move.stop()
             else:
-                move.left(random.uniform(0.1, 1), 1)
+                move.left(random.uniform(0.1, 0.5), 0.87)
                 move.stop()
             time.sleep(2)
             print("checking")
