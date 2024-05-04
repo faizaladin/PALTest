7da43713b06115fe09c787b0a43e1d19dcc69ed8
faizaladin <faizaladin365@gmail.com>
2024-05-03 22:02:26 -0400

    whoops

--- reset.py
+++ reset.py
@@ -4,25 +4,29 @@ import testcamera
 import move 
 import random
 
-robot_info = testcamera.calculate_orientation()
-
-while robot_info[1] > 5 and robot_info[1] < 355:
-    print(robot_info[1])
-    if robot_info[1] < 5 or robot_info[1] > 355:
-        break
-    elif robot_info[1] > 90:
-        move.right(random.uniform(0.2, 0.5), 0.85)
-        move.stop()
-    else:
-        move.left(random.uniform(0.2, 0.5), 0.85)
-        move.stop()
-    time.sleep(2)
-    print("checking")
+try:
     robot_info = testcamera.calculate_orientation()
 
-print("done")
-move.backward(6)
-move.forward(0.3, 0.25)
-move.right(1, 1)
-move.backward(6)
-move.stop()
+    while robot_info[1] > 5 and robot_info[1] < 355:
+        print(robot_info[1])
+        if robot_info[1] < 5 or robot_info[1] > 355:
+            break
+        elif robot_info[1] > 90:
+            move.right(random.uniform(0.2, 0.5), 0.85)
+            move.stop()
+        else:
+            move.left(random.uniform(0.2, 0.5), 0.85)
+            move.stop()
+        time.sleep(2)
+        print("checking")
+        robot_info = testcamera.calculate_orientation()
+
+    print("done")
+    move.backward(6)
+    move.forward(0.3, 0.25)
+    move.right(1, 1)
+    move.backward(6)
+    move.stop()
+
+except KeyboardInterrupt:
+    move.stop()
