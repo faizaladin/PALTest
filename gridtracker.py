a5ce654bb09adf118fb067a8afb5b36971fd52db
faizaladin <faizaladin365@gmail.com>
2024-05-05 15:18:23 -0400

    testing

--- gridtracker.py
+++ gridtracker.py
@@ -16,16 +16,19 @@ captured_images = []
 
 move.grid_forward(0.2)
 
+buffer_size = 20
+cap = cv2.VideoCapture('rtsp://admin:123456@136.244.195.47:554/Streaming/channels/0')  # Use 0 for the default camera
+cap.set(cv2.CAP_PROP_BUFFERSIZE, buffer_size)
+
 try:
+    move.grid_forward(0.25)
     while image_count < max_images:
-        buffer_size = 20
         # Initialize the camera
-        cap = cv2.VideoCapture('rtsp://admin:123456@136.244.195.47:554/Streaming/channels/0')  # Use 0 for the default camera
-        cap.set(cv2.CAP_PROP_BUFFERSIZE, buffer_size)
         # Capture frame-by-frame
         ret, frame = cap.read()
         captured_images.append([ret, frame])
         image_count += 1
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
     move.stop()
 
 finally:
