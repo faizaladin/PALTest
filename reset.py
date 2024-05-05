2c3f43b0cdcfce6d1114c95bc27f7acea7e48594
faizaladin <faizaladin365@gmail.com>
2024-05-05 19:56:06 -0400

    making faster

--- reset.py
+++ reset.py
@@ -19,7 +19,7 @@ def reset():
         robot_info = testcamera.calculate_orientation(ret, frame)
 
         while robot_info[1] > 5 and robot_info[1] < 355:
-            robot_info = testcamera.calculate_orientation(ret, frame)
+            #robot_info = testcamera.calculate_orientation(ret, frame)
             print(robot_info[1])
             if robot_info[1] < 5 or robot_info[1] > 355:
                 break
@@ -31,17 +31,17 @@ def reset():
                 move.stop()
             time.sleep(2)
             print("checking")
-            cap = cv2.VideoCapture('rtsp://admin:123456@136.244.195.47:554/Streaming/channels/0')  # Use 0 for the default camera
-            cap.set(cv2.CAP_PROP_BUFFERSIZE, buffer_size)
+            # cap = cv2.VideoCapture('rtsp://admin:123456@136.244.195.47:554/Streaming/channels/0')  # Use 0 for the default camera
+            # cap.set(cv2.CAP_PROP_BUFFERSIZE, buffer_size)
             ret, frame = cap.read()
             robot_info = testcamera.calculate_orientation(ret, frame)
 
         move.backward(8)
         move.forward(0.3, 0.25)
         move.stop()
-        print("working on turn")
-        cap = cv2.VideoCapture('rtsp://admin:123456@136.244.195.47:554/Streaming/channels/0')  # Use 0 for the default camera
-        cap.set(cv2.CAP_PROP_BUFFERSIZE, buffer_size)
+        # print("working on turn")
+        # cap = cv2.VideoCapture('rtsp://admin:123456@136.244.195.47:554/Streaming/channels/0')  # Use 0 for the default camera
+        # cap.set(cv2.CAP_PROP_BUFFERSIZE, buffer_size)
         ret, frame = cap.read()
         robot_info = testcamera.calculate_orientation(ret, frame)
         while robot_info[1] < 91 or robot_info[1] > 94:
@@ -54,8 +54,8 @@ def reset():
                 move.stop()
             time.sleep(2)
             print("checking")
-            cap = cv2.VideoCapture('rtsp://admin:123456@136.244.195.47:554/Streaming/channels/0')  # Use 0 for the default camera
-            cap.set(cv2.CAP_PROP_BUFFERSIZE, buffer_size)
+            # cap = cv2.VideoCapture('rtsp://admin:123456@136.244.195.47:554/Streaming/channels/0')  # Use 0 for the default camera
+            # cap.set(cv2.CAP_PROP_BUFFERSIZE, buffer_size)
             ret, frame = cap.read()
             robot_info = testcamera.calculate_orientation(ret, frame)
         move.backward(8)
