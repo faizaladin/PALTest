bddc94953d61089c6a542ad983a4fe7395fb9959
faizaladin <faizaladin365@gmail.com>
2024-05-06 12:30:22 -0400

    bug

--- move.py
+++ move.py
@@ -43,7 +43,7 @@ def right(num, en_value):
     motor_a.forward()
     motor_b.backward()
     time.sleep(num)
-    move.stop()
+    stop()
 
 def left(num, en_value):
     ena.value = en_value
@@ -51,7 +51,7 @@ def left(num, en_value):
     motor_a.backward()
     motor_b.forward()
     time.sleep(num)
-    move.stop()
+    stop()
 
 def curve_left_while_forward125():
     forward(0.25, 0.2)  # Move forward for 1 second at 50% speed
