caf4edaa5df64c73fffa49ad961dcdd7cb10178b
faizaladin <faizaladin365@gmail.com>
2024-05-14 21:05:27 -0400

    worth a shot

--- move.py
+++ move.py
@@ -57,8 +57,8 @@ def left(num, en_value):
 def curve_left_while_forward125():
     grids_hit = []
     image_count = 0
-    max_images_turn = 40
-    max_images_forward = 50
+    max_images_turn = 50
+    max_images_forward = 66
     buffer_size = 4
     captured_images = []
     cap = cv2.VideoCapture('rtsp://admin:123456@136.244.195.47:554/Streaming/channels/0')  # Use 0 for the default camera
@@ -90,7 +90,7 @@ def curve_left_while_forward125():
     # ret, frame = cap.read()
     # captured_images.append([ret, frame])
     for i in range(len(captured_images)):
-            if i % 5 == 0:
+            if i % 10 == 0:
                 #cv2.imshow(f"Photo {i}", captured_images[i][1])
                 # Display the image
                 info = testcamera.calculate_orientation(captured_images[i][0], captured_images[i][1])
