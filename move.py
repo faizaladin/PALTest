320c75578c4c0ce992690240ce6f15f9d94080c9
faizaladin <faizaladin365@gmail.com>
2024-05-16 17:15:43 -0400

    turn 6

--- move.py
+++ move.py
@@ -116,9 +116,16 @@ def curve_left_while_forward625():
     stop() 
 
 def curve_left_while_forward750():
-    forward(0.25, 0.2)  # Move forward for 1 second at 50% speed
-    left(0.50, 0.87) 
-    forward(4, 0.2)   # 
+    ena_value = 0.25
+    enb_value = 0.95
+    ena.value = ena_value
+    enb.value = enb_value
+    motor_a.backward()
+    motor_b.forward()
+    time.sleep(1.3)
+    grid_forward(0.2)
+    time.sleep(1.2)
+    stop()  
 
 def curve_left_while_forward875():
     forward(0.25, 0.2)  # Move forward for 1 second at 50% speed
@@ -170,8 +177,5 @@ def curve_right_while_forward1000():
     right(0.2, 0.87) 
     forward(4, 0.2)   # 
 
-forward(0.5, 0.25)
-stop()
-right(1.5, 0.87)
 #curve_left_while_forward250()
 #forward(5, 0.2)
