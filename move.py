882d7c997676aa2ff5ecd8b8ff2a097c77767434
faizaladin <faizaladin365@gmail.com>
2024-05-16 17:07:54 -0400

    b etter turn

--- move.py
+++ move.py
@@ -170,5 +170,8 @@ def curve_right_while_forward1000():
     right(0.2, 0.87) 
     forward(4, 0.2)   # 
 
+forward(0.25, 0.5)
+stop()
+right(2, 0.5)
 #curve_left_while_forward250()
 #forward(5, 0.2)
