dc5f3ba9b8208e2d27a2c1094939e5cb57103da8
faizaladin <faizaladin365@gmail.com>
2024-05-16 16:42:57 -0400

    my bad

--- reset.py
+++ reset.py
@@ -26,10 +26,10 @@ def reset():
             if robot_info[1] < 5 or robot_info[1] > 355:
                 break
             elif robot_info[1] <= 180:
-                move.right(random.uniform(0.15, 0.4), 0.87)
+                move.left(random.uniform(0.15, 0.4), 0.87)
                 move.stop()
             else:
-                move.left(random.uniform(0.15, 0.4), 0.87)
+                move.right(random.uniform(0.15, 0.4), 0.87)
                 move.stop()
             time.sleep(2)
             print("checking")
